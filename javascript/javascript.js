d9256e0539d1f85ca1816289bf81d524cc5b8a55
jthlesi <distanc87@gmail.com>
2022-06-29 19:00:25 +0900

    0629

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,69 +1,47 @@
 //풀페이지
+//페이저생성
 $(document).ready(function(){
-    fullCheck();
-    pageClick();
-})
+    var fullIdx = document.querySelectorAll("#fullpage .fullsection");
 
-function fullCheck(){
-    var fullIdx = document.querySelectorAll("#fullpage .fullsection")
     for (i=1;i<=fullIdx.length;i++){
         $("#fullpage > .page > ul").append("<li></li>");
     }
-    $(".page ul li:first-child").addClass("active");
 
+    $(".page ul li:first-child").addClass("active");
+});
+
+//페이저 클릭이동
+var	height = $(".fullsection").height();
+
+$(document).ready(function(){
+	$(".page li").click(function(){
+		var pageIdx = $(this).index();
+
+		$(this).addClass("active").siblings().removeClass("active");
+		
+		$("#fullpage").stop().animate({"top": -height * pageIdx + "px"},700, "swing");
+	});
+});
+
+//스크롤
+$(document).ready(function(){
     $(window).on('scroll touchmove mousewheel', function(event){
-		var page = $(".page ul li.active");
-		if($("body").find("#fullpage:animated").length >= 1) return false;
+		var pageActive = $(".page ul li.active");
+		var pageIndex = pageActive.index();
+		
 		if(event.originalEvent.wheelDelta >= 0) {
-			var before=page.index();
-			if(page.index() >= 0) page.prev().addClass("active").siblings(".active").removeClass("active");
-			var pagelength=0;
-			for(var i=1; i<(before); i++)
-			{
-				pagelength += $(".full"+i).height();
-			}
-			if(page.index() > 0){ 
-				page=page.index()-1;
-				$("#fullpage").animate({"top": -pagelength + "px"},1000, "swing");
+			if(pageIndex > 0) {
+				pageActive.prev().addClass("active").siblings().removeClass("active");
+				$("#fullpage").stop().animate({"top": -height*(pageIndex-1)+ "px"},700,"swing");
 			}
 		}else{
-			var nextPage=parseInt(page.index()+1);
-			var lastPageNum=parseInt($(".page ul li").length);
-			if(page.index() <= $(".page ul li").length-1){ 
-				page.next().addClass("active").siblings(".active").removeClass("active");
+			if(pageIndex <$(".page ul li").length-1){ 
+				pageActive.next().addClass("active").siblings().removeClass("active");
+				$("#fullpage").stop().animate({"top": -height*(pageIndex+1) + "px"},700,"swing");
 			}
-			
-			if(nextPage < lastPageNum){
-				var pagelength=0;
-				for(var i = 1; i<(nextPage+1); i++){ 
-					pagelength += $(".full"+i).height();
-				}
-				$("#fullpage").animate({"top": -pagelength + "px"},1000, "swing");
-			}
-            
-			
 		}
 	});
-}
-
-
-function pageClick(){
-	$(".page li").click(function(){
-		var gnbindex = $(this).index()+1;
-		var length=0;
-		for(var i=1; i<(gnbindex); i++)
-		{
-			length+=$(".full"+i).height();
-		}
-		if($("body").find("#fullpage:animated").length >= 1) return false;
-		$(this).addClass("active").siblings("li").removeClass("active");
-		
-		$("#fullpage").animate({"top": -length + "px"},800, "swing");
-		return false;
-	});
-}
-
-
+});
 
 //슬라이드
 var i=0;
@@ -83,3 +61,50 @@ setInterval(slide,3000)
 $(document).ready(function(){
     $(".cross").css("width",$(window).height())
 })
+
+//네비게이션
+$(function(){
+	$("header").hover(function(){
+		$(".nav_sub").stop().slideDown();
+		$("#nav_wrap, .nav_sub").css({"backgroundColor":"black","opacity":"0.9"})
+	}, function(){
+		$(".nav_sub").stop().slideUp("fast");
+		$("#nav_wrap, .nav_sub").css({"backgroundColor":"","opacity":""})
+	})
+})
+
+$(".sub>li").css({"width":$("nav>ul>li").width()})
+
+//서비스섹션 슬라이드
+$(".slides").css({"width":$(window).width()})
+
+var slideLeft = $(".slides").width();
+
+function prev(){
+	$(".slides:last").prependTo("#slideWrap_2");
+	$("#slideWrap_2").css("margin-left",slideLeft);
+	$("#slideWrap_2").stop().animate({marginLeft:0},800);
+}
+
+function next(){
+	$("#slideWrap_2").stop().animate({marginLeft:-slideLeft},800,function(){;
+		$(".slides:first").appendTo("#slideWrap_2");
+		$("#slideWrap_2").css("margin-left",0);
+	})
+}
+
+$("#prev").click(function(){
+	prev()
+});
+
+$("#next").click(function(){
+	next()
+});
+
+
+$("span").eq(0).text($(".slides").length);
+$("span").eq(1).text($(".slides").length);
+
+
+
+// setInterval(next,3000)
